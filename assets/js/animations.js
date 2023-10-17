c4e2f2846106d84d875e804af1304e9d6de03f6f
Schuzoo <119678593+ChristianDriz@users.noreply.github.com>
2023-10-17 18:41:28 +0800

    updates

---
+++ assets/js/animations.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+    
+//     const txt = $('.title');
+
+//     const txtload = () => {
+//         setTimeout(() => {
+//             txt.html('-Web Developer');
+//         }, 0);
+//         setTimeout(() => {
+//             txt.html('-Web Designer');
+//         }, 4000);
+//     }
+
+//     txtload();
+
+//     setInterval(txtload, 8000);
+// });
